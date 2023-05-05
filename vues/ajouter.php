ec51c793b2befa15ca93757496171913321134ab
Chengfc <chenf8787@gmail.com>
2023-05-05 13:24:37 -0400

    modif main.js url local fc

--- vues/ajouter.php
+++ vues/ajouter.php
@@ -1,22 +1,52 @@
-<div class="ajouter">
+<section class="ajouter">
 
+    <h1><span>Ajouter une bouteille au cellier </strong></span></h1>
     <div class="nouvelleBouteille" vertical layout>
-        <strong>Recherche : </strong><input type="text" name="nom_bouteille">
-        <ul class="listeAutoComplete">
+        <form action="">
+            <div class="input-box">
+                <label> Recherche :
+                    <input type="text" name="nom_bouteille">
+                </label>
+            </div>
 
-        </ul>
-        <div>
-            <p>Nom : <!-- <input data-id="" class="nom_bouteille" type="text" name="nom_bouteille" placeholder="Nom de la bouteille" disabled> -->
-                 <strong><span data-id="" class="nom_bouteille"></span></strong>
-            </p>
-            <p>Millesime (année): <input type="number" name="millesime" placeholder="1900"></p>
-            <p>Quantite : <input type="number" min="1" name="quantite" value="1"></p>
-            <p>Date achat : <input type="date" name="date_achat"></p>
-            <p>Prix : <input type="number" min="0" step="0.01" name="prix" placeholder="0.00"> $</p>
-            <p>Garde (année): <input type="number" name="garde_jusqua" placeholder="2025"></p>
-            <p>Notes :<input type="text" placeholder="Commentaire..." name="notes"></p>
-        </div>
-        <button name="ajouterBouteilleCellier">Ajouter la bouteille (champs tous obligatoires)</button>
+            <p class="hidden name_bottle">Nom : <span data-id="" class="nom_bouteille"></span></p>
+
+
+            <ul class="listeAutoComplete">
+
+            </ul>
+
+            <div class="input-box">
+                <label> Millesime <span class="obligatoire">*</span> :
+                    <input required type="number" name="millesime" >
+                </label>
+
+                <label> Quantite <span class="obligatoire">*</span>  :
+                    <input required type="number" step="0" name="quantite" >
+                </label>
+            </div>
+
+            <div class="input-box">
+                <label> Date achat <span class="obligatoire">*</span> :
+                    <input required type="date" name="date_achat">
+                </label>
+
+                <label> Prix <span class="obligatoire">*</span> :
+                    <input required type="number" step="0.01" name="prix">
+                </label>
+            </div>
+
+            <div class="input-box">
+                <label> Garde <span class="obligatoire">*</span> :
+                    <input required type="text" name="garde_jusqua">
+                </label>
+
+                <label> Notes <span class="obligatoire">*</span> :
+                    <input required type="text" name="notes">
+                </label>
+            </div>
+            <button name="ajouterBouteilleCellier" class="btn btnUpdate">Ajouter</button>
+            <p class="obligatoire_champ"><span class="obligatoire">*</span>Champs Tous Obligatoires</p>
+        </form>
     </div>
-</div>
-</div>
+</section>
