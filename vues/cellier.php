916406b36f3ae0b4cd6846c8b24bf50809103647
absahmed799 <absahmed799@gmail.com>
2023-05-05 11:08:50 -0400

    corriger l'importation du SAQ et la modification

--- vues/cellier.php
+++ vues/cellier.php
@@ -10,17 +10,15 @@ foreach ($data as $cle => $bouteille) {
         </div>
         <div class="description">
             <p class="nom">Nom : <?php echo $bouteille['nom'] ?></p>
-            <p class="quantite">Quantité :  <button class='btnBoire'>-</button><?php echo $bouteille['quantite'] ?><button class='btnAjouter'>+</button></p>
+            <p class="quantite">Quantité : <?php echo $bouteille['quantite'] ?>/p>
             <p class="pays">Pays : <?php echo $bouteille['pays'] ?></p>
             <p class="type">Type : <?php echo $bouteille['type'] ?></p>
             <p class="millesime">Millesime : <?php echo $bouteille['millesime'] ?></p>
             <p><a href="<?php echo $bouteille['url_saq'] ?>">Voir SAQ</a></p>
         </div>
         <div class="options" data-id="<?php echo $bouteille['id_bouteille_cellier'] ?>">
-            <button>Modifier</button>
-            
-           
-            
+        <a href="?requete=modifierBouteilleCellier&id=<?php echo$bouteille['id_bouteille_cellier']?>&nom=<?php echo $bouteille['nom'] ?>">Modifier</a>
+        <button class='btnBoire'>Boire</button><button class='btnAjouter'>Ajouter</button><
         </div>
     </div>
     <?php
--- vues/modifier.php
+++ vues/modifier.php
@@ -6,15 +6,16 @@
 
         </ul>
             <div >
-                <p>Nom : <span data-id="" class="nom_bouteille"></span></p>
-                <p>Millesime : <input name="millesime"></p>
-                <p>Quantite : <input name="quantite" value="1"></p>
-                <p>Date achat : <input name="date_achat"></p>
-                <p>Prix : <input name="prix"></p>
-                <p>Garde : <input name="garde_jusqua"></p>
-                <p>Notes <input name="notes"></p>
+                <p>Nom : <span data-id="" class="nom_bouteille"><?php echo $nom_bouteille ?></span></p>
+                <p>Millesime : <input name="millesime" value="<?php echo  $bouteilleInfo[0]['millesime']  ?>"></p>
+                <p>Quantite : <input name="quantite" value="<?php  echo $bouteilleInfo[0]['quantite']  ?>"></p>
+                <p>Date achat : <input name="date_achat" value="<?php  echo $bouteilleInfo[0]['date_achat']  ?>"></p>
+                <p>Prix : <input name="prix" value="<?php  echo $bouteilleInfo[0]['prix']  ?>"></p>
+                <p>Garde : <input name="garde_jusqua" value="<?php echo  $bouteilleInfo[0]['garde_jusqua']  ?>"></p>
+                <p>Notes <input name="notes" value="<?php echo  $bouteilleInfo[0]['notes']  ?>"></p>
+                <input type="hidden" name="id" value="<?php echo  $bouteilleInfo[0]['id']  ?>">
             </div>
-            <button name="ajouterBouteilleCellier">Ajouter la bouteille (champs tous obligatoires)</button>
+            <button name="modifierBouteilleCellier">Ajouter la bouteille (champs tous obligatoires)</button>
         </div>
     </div>
 </div>
