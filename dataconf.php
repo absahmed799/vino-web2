554c1c5ea20437e63c81b1ae85638011c69ce93e
Chengfc <chenf8787@gmail.com>
2023-05-05 12:50:39 -0400

    ajout du dataconf.php et modif .gitignore

---
+++ dataconf.php
@@ -0,0 +1,9 @@
+<?php
+	// Remplir et renommer dataconf.php
+	define("BASEURL", "");
+
+	define('HOST', 'localhost');
+	define('USER', 'root');
+	define('PASSWORD', 'root');
+	define('DATABASE', 'vinodb');
+?>
